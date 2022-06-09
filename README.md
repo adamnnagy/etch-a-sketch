4bfe020dd6c2acc63c9bd77c42c572ecf6103436
Adam Nagy <nagy.adamn@gmail.com>
2022-06-09 17:28:05 +0200

    Create grid

--- README.md
+++ README.md
@@ -11,6 +11,39 @@ Create a webpage with a 16x16 grid of square divs.
 - the event changes the color of the current target
 - hover on the grid items -- so i know i can interact with them if i go over them with my mouse -- could be a simple "border"
 
+creating a grid item
+1. get the size of the screen
+    screen.height -> func that returns that
+1. calculate how large the item should be
+    - height and width are the same
+    - this size is equal to the length of the grid container divided by the number of grid items in a row - which is basically the grid size
+1. create one grid item (pixel?) with the calculated size 
+1. add a class that defines it as a grid item (pixel?)
+1. create as many of them as needed -> size * size
+1. add these into an array
+1. add this array to the grid container (screen?)
+
+PROBLEM!
+creating and adding new grid items really slows down the browser
+SOLUTION
+instead of creating new divs, check how many there are, and either add or remove items from the array, while resizing the items
+1. if there are more items in the current grid
+    1. remove the items that overflow
+    1. resize the items that should stay
+1. if there are less items
+    1. add new items
+        1. create an item
+        1. make the size appropriate
+        1. multiply the items -- so I add the difference * difference items
+    1. resize the items that are already there
+
+resize function - loops over the items and changes the width and height
+- resize each time!
+to add / remove - could use the .splice() function on the gridItems array.
+    when adding - .splice(array.length, 0, [...newItems])
+    when removing - .splice(array.length, difference)
+
+
 
 ### different brushes
 - classic gray
--- js/script.js
+++ js/script.js
@@ -1,12 +1,87 @@
 const gridSizeSlider = document.querySelector("#grid-size");
-const gridSize = gridSizeSlider.value;
+const getNewGridSize = () => Number(gridSizeSlider.value);
 
-const gridSizeDisplay = document.querySelector('#grid-size-display')
+let currentSize = 0;
+const getCurrentGridSize = () => Number(currentSize);
+const setCurrentGridSize = (size) => (currentSize = size);
+
+const sliderLabel = document.querySelector("#grid-size-display");
 
 const displayGridSize = (gridSize) => {
-    gridSizeDisplay.textContent = `${gridSize}x${gridSize}`;
-}
+	sliderLabel.textContent = `${gridSize}x${gridSize}`;
+};
 
-gridSizeSlider.addEventListener('change', e => {
-    displayGridSize(e.target.value);
-})
+const gridContainer = document.querySelector("#grid-container");
+
+const getGridItemSize = (gridSize, gridContainer) => {
+	const containerWidth = gridContainer.offsetWidth;
+	const itemSize = containerWidth / gridSize;
+	return itemSize;
+};
+
+const createGridItem = (size) => {
+	const gridItem = document.createElement("div");
+	gridItem.classList.add("grid-item");
+	gridItem.style.width = `${size}px`;
+	gridItem.style.height = `${size}px`;
+	return gridItem;
+};
+
+const resizeItem = (item, size) => {
+	item.style.width = `${size}px`;
+	item.style.height = `${size}px`;
+};
+
+const isLargerGrid = (currSize, newSize) => {
+	return currSize < newSize;
+};
+
+const getSizeDifference = (currSize, newSize) => Math.abs(currSize - newSize);
+const addGridItemToContainer = (item) => {
+	gridContainer.appendChild(item);
+};
+
+const createGrid = (newSize) => {
+	let gridItems = [...document.querySelectorAll(".grid-item")];
+	gridContainer.innerHTML = "";
+	const currSize = getCurrentGridSize();
+	const diff = getSizeDifference(currSize, newSize);
+	gridItems.forEach((i) =>
+		resizeItem(i, getGridItemSize(newSize, gridContainer))
+	);
+	if (isLargerGrid(currSize, newSize)) {
+		for (let i = 0; i < newSize * newSize - currSize * currSize; i++) {
+			gridItems.push(
+				createGridItem(getGridItemSize(newSize, gridContainer))
+			);
+		}
+	} else {
+		gridItems = gridItems.slice(0, newSize * newSize);
+	}
+
+	gridItems.forEach((i) => addGridItemToContainer(i));
+
+	setCurrentGridSize(newSize);
+};
+
+const resizeGrid = (newSize) => {
+	const gridItems = [...document.querySelectorAll(".grid-item")];
+	const resized = gridItems.map((item) => {
+		item.style.height = `${newSize}px`;
+		item.style.width = `${newSize}px`;
+	});
+	return resized;
+};
+
+gridSizeSlider.addEventListener("change", (e) => {
+	displayGridSize(e.target.value);
+	createGrid(getNewGridSize());
+	console.log(getCurrentGridSize(), getNewGridSize());
+});
+
+const initialRun = () => {
+	createGrid(getNewGridSize());
+	setCurrentGridSize(getNewGridSize());
+};
+
+initialRun();
